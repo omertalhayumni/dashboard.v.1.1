f2a69fe0e606e83f03e9843f4647170300be1dd5
omertalhayumni <yumnitalha@gmail.com>
2023-01-27 17:14:13 +0300

    mytt

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./chart.scss";
 
 const Chart = () => {
-  return <div className="chart">Chart</div>;
+  return <div className="chart">aa</div>;
 };
 
 export default Chart;
